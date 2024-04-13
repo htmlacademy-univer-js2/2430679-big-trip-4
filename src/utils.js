c488836aadea080d802bedc42d1dbad8af4aebbc
Marina <Marina.Apkaeva@urfu.me>
2024-04-14 02:00:28 +0500

    Шаблонизируй то

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,42 @@
 import dayjs from 'dayjs';
 
-function getRandomArrayElement(array) {
+const getRandomArrayElement = (array) => {
   return array[Math.floor(Math.random() * array.length)];
 }
 
-function getTimeInHours(startTime, endTime) {
+const getTimeInHours = (startTime, endTime) => {
   const hours = dayjs(endTime).diff(dayjs(startTime), 'hours');
   return hours !== 0 ? `${hours}H` : '';
 }
 
-function getTimeInMinutes(startTime, endTime) {
+const getTimeInMinutes = (startTime, endTime) => {
   const minutes = dayjs(endTime).diff(dayjs(startTime), 'minutes') % 60;
   return minutes !== 0 ? `${minutes}M` : '';
 }
 
-export { getRandomArrayElement, getTimeInHours, getTimeInMinutes };
+const getTripTitle = (cities) => {
+  return cities.reduce((acc, city, index) => {
+    if (index !== cities.length - 1) {
+      acc += `${city} &mdash; `;
+    } else {
+      acc += `${city}`;
+    }
+    return acc;
+  }, '');
+}
+
+const getTripStart = (sortedPoints) => {
+  return dayjs(sortedPoints[0].date.startTime).format('MMM DD');
+}
+
+const getTripEnd = (sortedPoints) => {
+  const startDate = sortedPoints[0].date.startTime;
+  const endDate = sortedPoints[sortedPoints.length - 1].date.endTime;
+  if (dayjs(startDate).format('MMM') === dayjs(endDate).format('MMM')) {
+    return dayjs(endDate).format('DD');
+  } else {
+    return dayjs(endDate).format('MMM DD');
+  }
+}
+
+export {getRandomArrayElement, getTimeInHours, getTimeInMinutes, getTripTitle, getTripStart, getTripEnd};
