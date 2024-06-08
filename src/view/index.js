bd1f234944219345796052ac2df8a261b7b0ce9d
Марина Юрьевна Апкаева <132090822+WilgardWolf@users.noreply.github.com>
2024-06-08 18:37:00 +0500

    fix

--- src/view/index.js
+++ src/view/index.js
@@ -1,23 +1,12 @@
 import EditPointView from './edit-point-view';
-import FilterView from './filter-view';
-import PointListView from './point-list-view';
-import PointView from './point-view';
-import TripInfoView from './trip-info-view';
-import SortView from './sort-view';
 import EmptyListView from './empty-list-view';
-import NewPointButtonView from './new-point-button-view';
-import LoadingView from './loading-view';
 import ErrorView from './error-view';
+import FilterView from './filter-view';
+import LoadingView from './loading-view';
+import NewPointButtonView from './new-point-button-view';
+import PointView from './point-view';
+import PointListView from './point-list-view';
+import SortView from './sort-view';
+import TripInfoView from './trip-info-view';
 
-export {
-  EditPointView,
-  FilterView,
-  PointListView,
-  PointView,
-  TripInfoView,
-  SortView,
-  EmptyListView,
-  NewPointButtonView,
-  LoadingView,
-  ErrorView,
-};
+export { EditPointView, EmptyListView, ErrorView, FilterView, LoadingView, NewPointButtonView, PointView, PointListView, SortView, TripInfoView };
